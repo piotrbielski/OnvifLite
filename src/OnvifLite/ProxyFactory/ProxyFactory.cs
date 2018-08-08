b7ff0270dbd8f4efa320317da0f00489a98d3aec
Piotr Bielski <bpiotrrr@gmail.com>
2018-08-08 22:40:37 +0200

    Created proxy factory and project for test

---
+++ src/OnvifLite/ProxyFactory/ProxyFactory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel.Channels;
+using System.ServiceModel;
+using System.Text;
+using System.Net;
+
+namespace OnvifLite.ProxyFactory
+{
+    internal class ProxyFactory<TContract, TService> 
+        where TContract : class
+        where TService : ClientBase<TContract>, TContract
+    {
+        private readonly CustomBinding _binding;
+
+        public ProxyFactory()
+        {
+            var messageBindingElement = new TextMessageEncodingBindingElement()
+            {
+                MessageVersion = MessageVersion.CreateVersion(EnvelopeVersion.Soap12, AddressingVersion.WSAddressing10)
+            };
+
+            var transportBindingElement = new HttpTransportBindingElement()
+            {
+                AuthenticationScheme = AuthenticationSchemes.Digest
+            };
+
+            _binding = new CustomBinding(messageBindingElement, transportBindingElement);
+        }
+
+        public TContract CreateProxy(EndpointAddress address)
+        {
+            var proxy = (TContract)Activator.CreateInstance(typeof(TService), _binding, address);
+            return proxy;
+        }
+    }
+}
