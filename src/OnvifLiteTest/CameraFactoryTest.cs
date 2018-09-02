de1548aa19400edbe5b741313ae8bf694dcebdcc
Piotr Bielski <bpiotrrr@gmail.com>
2018-09-02 19:12:27 +0200

    Created CameraFactoryTest

---
+++ src/OnvifLiteTest/CameraFactoryTest.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OnvifLite;
+using OnvifLite.CameraState;
+using OnvifLite.Exceptions;
+using System.Net;
+
+namespace OnvifLiteTest
+{
+    [TestClass]
+    public class CameraFactoryTest
+    {
+        [TestMethod]
+        public void CreateNotConnectedCameraTest()
+        {
+            var ipAddress = IPAddress.Parse("192.168.0.1");
+            var cameraFactory = new CameraFactory();
+            var camera = cameraFactory.CreateCamera(ipAddress);
+
+            Assert.AreEqual(ipAddress, camera.IPAddress);
+            Assert.AreEqual(camera.State, CameraStateEnum.NotConnected);
+        }
+
+        [TestMethod]
+        public void CreateNotConnectedCameraExceptionTest()
+        {
+            var ipAddress = IPAddress.Parse("192.168.0.1");
+            var cameraFactory = new CameraFactory();
+
+            try
+            {
+                var camera = cameraFactory.CreateCamera("admin", "admin", ipAddress);
+                Assert.Fail();
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual(typeof(CameraConnectionException), e.GetType());
+            }            
+        }
+    }
+}
